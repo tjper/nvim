1f1180b4eae479f3e5cb4efa40d7f62ddd1cbf52
James Perry <james@wyndlabs.ai>
2025-12-08 22:24:26 -0600

    overhauled nvim config to use new builtin lsp commands and better organization

--- lua/plugins/trouble.lua
+++
@@ -1,8 +0,0 @@
-local trouble_status_ok, trouble = pcall(require, 'trouble')
-if not trouble_status_ok then
-  return
-end
-
-trouble.setup({
-  opts = {}, -- for default options, refer to the configuration section for custom setup.
-})
